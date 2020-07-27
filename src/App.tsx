03d18dc542ff1405cd02a0485ca17b8b196e6b5a
刘博恒 <liuboheng@bytedance.com>
2020-07-28 00:33:37 +0800

    feat: init list

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import VirtualList from './components/virtual-list';
+
+class MyList extends VirtualList<string> {}
 
 function App() {
+  const data = ['1','2','3'];
+  const renderItem = (item: string) => item;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyList data={data} renderItem={renderItem} />
     </div>
   );
 }
---
+++ src/components/virtual-list/VirtualList.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface VLProps<T> extends React.HTMLAttributes<HTMLUListElement> {
+  data: T[];
+  renderItem: (item: T) => React.ReactNode;
+}
+
+class VirtualList<T> extends React.Component<VLProps<T>, {}> {
+  render() {
+    const { data, renderItem} = this.props;
+    return (
+      <ul>
+        {data.map(item => <li>{renderItem(item)}</li>)}
+      </ul>
+    )
+  }
+}
+
+export default VirtualList;
